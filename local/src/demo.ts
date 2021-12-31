1438c5c184d0cee6e885917a128a1e69121387bd
Nie Xiaojun <nxjniexiao@163.com>
2021-12-31 15:44:36 +0800

    feat: 测试markdown和html转doc

--- local/src/demo.ts
+++ local/src/demo.ts
@@ -1,5 +1,6 @@
 import * as wizEditorClient from 'wiz-editor/client';
 import { createEditorPromiseWrapper } from './create-editor-promise-wrapper';
+import { htmlString, markdownString } from './strings';
 
 // 测试数据回显
 const initLocalData = require('./initial-data.json');
@@ -7,21 +8,37 @@ createEditorPromiseWrapper(document.querySelector('#editor-2') as HTMLElement, {
   initLocalData,
 });
 
-// 测试只读模式
+// 测试 text
+const textData = wizEditorClient.text2doc('???');
 createEditorPromiseWrapper(
   document.querySelector('#node-1') as HTMLElement,
   {
-    initLocalData: wizEditorClient.text2doc('???'),
+    initLocalData: textData,
   },
   { permission: 'w' }
 );
 
+// 测试markdown
+const markdownData = wizEditorClient.markdown2Doc(markdownString);
 createEditorPromiseWrapper(
   document.querySelector('#node-2') as HTMLElement,
   {
-    initLocalData,
+    initLocalData: markdownData,
   },
-  { permission: 'r' }
+  { permission: 'w' }
+);
+
+// 测试 html
+const htmlData = wizEditorClient.html2Doc(htmlString, {
+  convertList: false,
+  convertFont: false,
+});
+createEditorPromiseWrapper(
+  document.querySelector('#node-3') as HTMLElement,
+  {
+    initLocalData: htmlData,
+  },
+  { permission: 'w' }
 );
 
 // TODO: 测试api
---
+++ local/src/strings.ts
@@ -0,0 +1,64 @@
+export const htmlString = `
+<h3>测试svg:</h3>
+<svg class="" focusable="false" viewBox="0 0 1024 1024" aria-hidden="true" role="presentation" t="1612235315010" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2406">
+  <path d="M932 512a408 408 0 0 1-80.4 246.9 412.5 412.5 0 0 1-206.7 151.8 25.2 25.2 0 0 1-21.6-3.9 21.3 21.3 0 0 1-6.6-16.5v-115.2a98.1 98.1 0 0 0-28.5-77.7 390 390 0 0 0 55.8-9.6 204.6 204.6 0 0 0 51.3-21.6 154.5 154.5 0 0 0 44.1-36 172.5 172.5 0 0 0 30-57.6 275.4 275.4 0 0 0 11.4-82.5 158.1 158.1 0 0 0-43.5-112.5 145.2 145.2 0 0 0-4.2-111.6 71.7 71.7 0 0 0-44.1 6 281.7 281.7 0 0 0-50.4 24l-20.7 12.9a390 390 0 0 0-210 0c-6-4.2-13.5-8.7-23.1-14.7A305.7 305.7 0 0 0 338 272a77.7 77.7 0 0 0-47.1-7.5 147 147 0 0 0-3.9 112.5 162.9 162.9 0 0 0-43.2 113.1A270 270 0 0 0 255.2 572a184.2 184.2 0 0 0 30 57.6 142.5 142.5 0 0 0 44.1 36.6 251.1 251.1 0 0 0 51.3 21.6 388.2 388.2 0 0 0 56.1 9.6 86.7 86.7 0 0 0-27 56.4 93 93 0 0 1-24.9 7.8 156.9 156.9 0 0 1-30 2.7A64.8 64.8 0 0 1 316.4 752a102.6 102.6 0 0 1-30-34.2 90 90 0 0 0-26.4-28.5 75.6 75.6 0 0 0-27-13.2h-10.8a39 39 0 0 0-15.9 2.4q-4.5 2.7-2.7 6.3a41.4 41.4 0 0 0 5.1 7.5 51.9 51.9 0 0 0 7.2 6.9l3.9 2.4a73.2 73.2 0 0 1 24 20.7 147.9 147.9 0 0 1 17.1 27.6l5.4 12.6a67.8 67.8 0 0 0 24 33.9 90 90 0 0 0 36.3 15.6 183.9 183.9 0 0 0 37.8 3.9 170.7 170.7 0 0 0 30-2.1l12.6-2.1v78.6a21.6 21.6 0 0 1-7.2 16.5 26.1 26.1 0 0 1-21.9 3.9 410.7 410.7 0 0 1-205.5-152.7A401.1 401.1 0 0 1 92 512a410.1 410.1 0 0 1 56.4-210A415.8 415.8 0 0 1 302 148.4 410.1 410.1 0 0 1 512 92a410.1 410.1 0 0 1 210 56.4A415.8 415.8 0 0 1 875.6 302 409.5 409.5 0 0 1 932 512z" p-id="2407">
+  </path>
+</svg>
+<h3>测试图片：</h3>
+<img class="main-logo" src="https://www.dell.com/zh-cn/outlet/media/logo/stores/1/dell_logo_2020_40px.png" alt="Dell Outlet for home" width="50" height="50">
+<h3>测试大段HTML：</h3>
+<span style="font-size: 20px">本题文字版整理</span>
+<div><span style="font-size: 20px"><br></span></div>
+<div><span style="font-size: 20px">本题考点一：算术平方根。算术平方根是平方根中正的那个。</span></div>
+<div><span style="font-size: 20px">平方根的定义是从平方来的，\(y=\sqrt{x}\)的含义就是\(y^2=x\)，</span></div>
+<div><span style="font-size: 20px">其中\(y^{2}=y\times y\)。</span><font style="font-size: 12px">如果乘法不懂，就继续看书和总结，</font></div>
+<div><font style="font-size: 12px">找到用来定义乘法的更加基本的概念，例如“乘法是重复的加法的简便计算”，</font></div>
+<div><font style="font-size: 12px">也就是\(a\times b = \underbrace{a+a+\cdots+a}_{b}\)。如果加法不懂就继续</font></div>
+<div><font style="font-size: 12px">搞清楚加法的定义，例如，加法是合起来数一数。</font></div>
+<div><span><br></span></div>
+<div>
+  <span style="font-size: 20px">本题考点二：有理数。有理数是无限循环小数，例如\(a.b\dot{c}\)。</span>
+  <span style="font-size: 20px">注意有限小数也是</span>
+</div>
+<div>
+  <span style="font-size: 20px">无限循环小数，循环的部分是最后的\(0\)。无限循环小数</span>
+  <span style="font-size: 20px">可以表示为分数\(\frac{q}{p}\)。</span>
+</div>
+<div><span style="font-size: 20px">如果这个“可以”不理解，则要继续通过看书、找人问和思考搞清楚。</span></div>
+<div><font style="font-size: 12px">例如，我们来看看\(0.\dot{c}\)怎么变成一个分数。设\(x=0.\dot{c}\)，则\(10x=c.\dot{c}\)，</font></div>
+<div><font style="font-size: 12px">于是\(10x-x=c\)，也就是\(9x=c\)，所以\(x=\frac{c}{9}\)，成了分数。我们还可以证明</font></div>
+<div><font style="font-size: 12px">任何一个分数\(\frac{q}{p}\)可以写成无限循环小数。如果不清楚这个“可以证明”，</font></div>
+<div><font style="font-size: 12px">同样通过看书问和思考搞清楚。我们来看看这个\(\frac{q}{p}\)，也就是\(q\)</font></div>
+<div><font style="font-size: 12px">除以\(p\)。首先，如果\(q&gt;p\)则有整数部分，得到的余数除以\(p\)才是小数部分。</font></div>
+<div><font style="font-size: 12px">我们主要看小数部分。这个小数部分的计算还是一样，每次凑一个商，算乘法，得到余数，然后重复。</font></div>
+<div>如果中间某一次余数是0，那就算完了，也就是有限小数，或者说无线循环的部分是0。如果余数重复了，</div>
+<div>那么，这个重复的位置后面的所有结果就会和重复之前的一样，也就是无限循环小数。现在，我们只要</div>
+<div>证明于是肯定会重复就行了。余数小于p，也就是说，如果每个不是零的余数不重复，最多出现\(p-1\)次，</div>
+<div>也就是余数为\(1,2,\cdots, p-1\)。那继续除下去，如果余数为0，则有限小数，也就是循环为0的无限循环小数；</div>
+<div>如果余数不为0，则肯定是前面已经出现过的那些余数之一，也就是余数重复出现了。既然余数重复出现了，</div>
+<div>商肯定也重复。因此，肯定是循环小数。</div>
+<div><br></div>
+<div><font style="font-size: 20px">本题考点三：无理数。除了有理数之外的数就是无理数，也就是无限不循环</font></div>
+<div>
+  <font style="font-size: 20px">小数，</font>
+  <span style="font-size: 20px">也就是不能写成分数\(\frac{q}{p}\)的数。</span>
+  <font style="font-size: 12px">于是，如果我们判断一个平方根</font>
+</div>
+<div>
+  <font style="font-size: 12px">
+    是不是一个无理数，我们只需要看看是不是\(\sqrt{x}=\frac{q}{p}\)。如果\(x\)自己是一个无理数，那\(\sqrt{x}\)就肯定是无理数了。
+  </font>
+</div>
+<div><font style="font-size: 12px">如果\(x\)是一个有理数，那就把\(x\)写成一个分数，然后看看这个分数的分子和分母是不是能够</font></div>
+<div><font style="font-size: 12px">写成一个平方数（平方数的定义是一个整数\(a\)满足\(a=b\times b\)，\(b\)也是整数）。</font></div>
+<div><span style="font-size: 20px"><br></span></div>
+<div><span style="font-size: 20px">本题考点四：代数的思想——用字母和算式来表示对象、关系、过程。</span></div>
+<div><span style="font-size: 20px">也就是把题中的过程写成：</span></div>
+<div><span style="font-size: 20px">输入\(x\)，计算\(y=\sqrt{x}\)，判断\(y\)是不是有理数，如果不是有理数，</span></div>
+<div><span style="font-size: 20px">则\(x=x+3\)，如果是有理数，则输出\(y\)。</span></div>
+<div><span style="font-size: 20px">在写的过程中，尽量把所有的东西都写成字母和算式，</span></div>
+<div><span style="font-size: 20px">要非常注意过程中每个时刻的计算和操作，到底在哪个变量的身上。</span></div>
+<div><font style="font-size: 20px"><br></font></div>
+`;
+
+export const markdownString = '* 无序列表1-1\n* 无序列表1-2\n\n1. 有序列表1-1\n2. 有序列表1-2\n\n\n| col1 | col2 | col3 |\n| - | - | - |\n| 1 | 2 | 3 |\n| 4 | 5 | 6 |\n\n* [X] checklist1-1\n* [ ] checklist1-2\n\n# H1标题\n\n## H2标题\n\n### H3标题\n\n#### H4标题\n\n##### H5标题\n\n###### H6标题\n\n🚀️ **多人协作**编辑器，支持*所见即所得*编辑模式和 Markdown 语法渲染~~可私有部署~~，利用自定义文档组件，与企业内部系统深度集成只需 2 分钟，让内部系统处处都可协同编辑立即体验 Demo，或马上尝试部署 `<a href=\"https://github.com/WizTeam/wiz-editor\" target=\"_blank\">GitHub</a>`。\n\n详情点击[Github](https://github.com/WizTeam/wiz-editor)。👍 🚀️\n\n$$\nx = {-b \\pm \\sqrt{b^2-4ac} \\over 2a}.\n\n$$\n\n```html\n<div>\n  test\n  <div>test2</div>\n</div>\n```\n';
